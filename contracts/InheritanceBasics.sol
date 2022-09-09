9b3999125606826d5cedfb416df4b9ccd91b39bb
walterz <tanisssss@protonmail.com>
2022-09-09 16:30:12 -0300

    Code cleanup

--- contracts/InheritanceBasics.sol
+++ contracts/InheritanceBasics.sol
@@ -13,27 +13,12 @@ contract Owned {
 
 contract InheritanceBasics is Owned {
 
-    mapping(address => bool) public myMap;
-    mapping(uint => mapping (uint => bool)) mapmap;
-
-
     constructor () {
         // Owned.constructor is automatically called
     }
 
     function getOwner () public view returns (address) {
         return owner;
-    }
-
-    function setTrueForAddress () public {
-        myMap[msg.sender] = true;
-    }
-
-    function getMapping (uint _index, uint _index2) public view returns (bool _value) {
-        return mapmap[_index][_index2];
-    }
-    function setMapping (uint _index, uint _index2, bool _value) public {
-        mapmap[_index][_index2] = _value;
     }
 
 }
