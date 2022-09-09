0aa0b2296c62533617a8e523368f44897de1f54c
walterz <tanisssss@protonmail.com>
2022-09-09 00:51:02 -0300

    Exceptions, try/catch and Events

--- contracts/Exceptions.sol
+++ contracts/Exceptions.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.9;
 
 contract Exceptions {
 
+    event LogError(string reason);
+
     mapping (address => uint) internal mapAddrBalance;
 
     constructor () {
@@ -26,17 +28,40 @@ contract Exceptions {
         return mapAddrBalance[_from];
     }
 
-    function withdrawMoney(address payable _to, uint ammount) public payable {
+    function withdrawMoney(address payable _to, uint _amount) public {
+
         // 'require' is preferred for input validation
-        require (mapAddrBalance[_to] > ammount, "You don't have that much balance in this SC.");
+        // if tx is reverted, remaining gas is returned to the sender
+        require(_amount <= mapAddrBalance[msg.sender], "Not Enough Funds, aborting");
 
         // assert should be used for checking invariants (illegal variable values)
         // for example, a uint that gets to a value less than zero, will wrap around
         // setting it's value to more than it should've
-        assert (mapAddrBalance[_to] > mapAddrBalance[_to] - ammount);
+        // assert consumes all tx gas
+        assert(mapAddrBalance[msg.sender] >= mapAddrBalance[msg.sender] - _amount);
 
-        mapAddrBalance[_to] -= ammount;
-        _to.transfer(ammount);
+        mapAddrBalance[msg.sender] -= _amount;
+        _to.transfer(_amount);
     }
 
+    function tryCatcTest () public {
+        
+        WillFail will = new WillFail();
+
+        try will.fail() {
+            // do something (?)
+        }  catch Error(string memory reason) {
+            emit LogError(reason);
+        }
+    }
+
+}
+
+// inline external contract to test exception catching and events
+contract WillFail {
+
+    // pure functions do not read or modify the state
+    function fail() public pure {
+        require(false, "Error message.");
+    }
 }
